72c85904edb414a9870afec4c0039d0efc3acf6c
bhawk3 <brendanhawk03@aol.com>
2020-10-29 09:48:36 -0400

    Add project files.

---
+++ 4.6.cs.piecework/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace _4._6.cs.piecework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.summaryButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.clearAllButton = new System.Windows.Forms.Button();
+            this.name1TextBox = new System.Windows.Forms.TextBox();
+            this.name2TextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.piecesCompleted1TextBox = new System.Windows.Forms.TextBox();
+            this.piecesCompleted2TextBox = new System.Windows.Forms.TextBox();
+            this.piecesCompletedLabel = new System.Windows.Forms.Label();
+            this.amountEarned1TextBox = new System.Windows.Forms.TextBox();
+            this.amountEarned2TextBox = new System.Windows.Forms.TextBox();
+            this.amountEarnedLabel = new System.Windows.Forms.Label();
+            this.totalPiecesTextBox = new System.Windows.Forms.TextBox();
+            this.totalPayTextBox = new System.Windows.Forms.TextBox();
+            this.toalPiecesLabel = new System.Windows.Forms.Label();
+            this.totalPayLabel = new System.Windows.Forms.Label();
+            this.averagePayTextBox = new System.Windows.Forms.TextBox();
+            this.averagePayLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(542, 359);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(96, 23);
+            this.calculateButton.TabIndex = 0;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // summaryButton
+            // 
+            this.summaryButton.Location = new System.Drawing.Point(665, 359);
+            this.summaryButton.Name = "summaryButton";
+            this.summaryButton.Size = new System.Drawing.Size(96, 23);
+            this.summaryButton.TabIndex = 1;
+            this.summaryButton.Text = "Summary";
+            this.summaryButton.UseVisualStyleBackColor = true;
+            this.summaryButton.Click += new System.EventHandler(this.summaryButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(542, 397);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(96, 23);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // clearAllButton
+            // 
+            this.clearAllButton.Location = new System.Drawing.Point(665, 397);
+            this.clearAllButton.Name = "clearAllButton";
+            this.clearAllButton.Size = new System.Drawing.Size(96, 23);
+            this.clearAllButton.TabIndex = 3;
+            this.clearAllButton.Text = "Clear All";
+            this.clearAllButton.UseVisualStyleBackColor = true;
+            this.clearAllButton.Click += new System.EventHandler(this.clearAllButton_Click);
+            // 
+            // name1TextBox
+            // 
+            this.name1TextBox.Location = new System.Drawing.Point(70, 83);
+            this.name1TextBox.Name = "name1TextBox";
+            this.name1TextBox.Size = new System.Drawing.Size(100, 22);
+            this.name1TextBox.TabIndex = 4;
+            // 
+            // name2TextBox
+            // 
+            this.name2TextBox.Location = new System.Drawing.Point(70, 123);
+            this.name2TextBox.Name = "name2TextBox";
+            this.name2TextBox.Size = new System.Drawing.Size(100, 22);
+            this.name2TextBox.TabIndex = 5;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(89, 52);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(45, 17);
+            this.nameLabel.TabIndex = 6;
+            this.nameLabel.Text = "Name";
+            // 
+            // piecesCompleted1TextBox
+            // 
+            this.piecesCompleted1TextBox.Location = new System.Drawing.Point(202, 83);
+            this.piecesCompleted1TextBox.Name = "piecesCompleted1TextBox";
+            this.piecesCompleted1TextBox.Size = new System.Drawing.Size(100, 22);
+            this.piecesCompleted1TextBox.TabIndex = 7;
+            // 
+            // piecesCompleted2TextBox
+            // 
+            this.piecesCompleted2TextBox.Location = new System.Drawing.Point(202, 123);
+            this.piecesCompleted2TextBox.Name = "piecesCompleted2TextBox";
+            this.piecesCompleted2TextBox.Size = new System.Drawing.Size(100, 22);
+            this.piecesCompleted2TextBox.TabIndex = 8;
+            // 
+            // piecesCompletedLabel
+            // 
+            this.piecesCompletedLabel.AutoSize = true;
+            this.piecesCompletedLabel.Location = new System.Drawing.Point(199, 52);
+            this.piecesCompletedLabel.Name = "piecesCompletedLabel";
+            this.piecesCompletedLabel.Size = new System.Drawing.Size(121, 17);
+            this.piecesCompletedLabel.TabIndex = 9;
+            this.piecesCompletedLabel.Text = "Pieces Completed";
+            // 
+            // amountEarned1TextBox
+            // 
+            this.amountEarned1TextBox.Location = new System.Drawing.Point(335, 83);
+            this.amountEarned1TextBox.Name = "amountEarned1TextBox";
+            this.amountEarned1TextBox.Size = new System.Drawing.Size(100, 22);
+            this.amountEarned1TextBox.TabIndex = 10;
+            // 
+            // amountEarned2TextBox
+            // 
+            this.amountEarned2TextBox.Location = new System.Drawing.Point(335, 123);
+            this.amountEarned2TextBox.Name = "amountEarned2TextBox";
+            this.amountEarned2TextBox.Size = new System.Drawing.Size(100, 22);
+            this.amountEarned2TextBox.TabIndex = 11;
+            // 
+            // amountEarnedLabel
+            // 
+            this.amountEarnedLabel.AutoSize = true;
+            this.amountEarnedLabel.Location = new System.Drawing.Point(329, 52);
+            this.amountEarnedLabel.Name = "amountEarnedLabel";
+            this.amountEarnedLabel.Size = new System.Drawing.Size(106, 17);
+            this.amountEarnedLabel.TabIndex = 12;
+            this.amountEarnedLabel.Text = "Amount Earned";
+            // 
+            // totalPiecesTextBox
+            // 
+            this.totalPiecesTextBox.Location = new System.Drawing.Point(70, 288);
+            this.totalPiecesTextBox.Name = "totalPiecesTextBox";
+            this.totalPiecesTextBox.Size = new System.Drawing.Size(100, 22);
+            this.totalPiecesTextBox.TabIndex = 13;
+            // 
+            // totalPayTextBox
+            // 
+            this.totalPayTextBox.Location = new System.Drawing.Point(202, 288);
+            this.totalPayTextBox.Name = "totalPayTextBox";
+            this.totalPayTextBox.Size = new System.Drawing.Size(100, 22);
+            this.totalPayTextBox.TabIndex = 14;
+            // 
+            // toalPiecesLabel
+            // 
+            this.toalPiecesLabel.AutoSize = true;
+            this.toalPiecesLabel.Location = new System.Drawing.Point(67, 258);
+            this.toalPiecesLabel.Name = "toalPiecesLabel";
+            this.toalPiecesLabel.Size = new System.Drawing.Size(86, 17);
+            this.toalPiecesLabel.TabIndex = 15;
+            this.toalPiecesLabel.Text = "Total Pieces";
+            // 
+            // totalPayLabel
+            // 
+            this.totalPayLabel.AutoSize = true;
+            this.totalPayLabel.Location = new System.Drawing.Point(208, 258);
+            this.totalPayLabel.Name = "totalPayLabel";
+            this.totalPayLabel.Size = new System.Drawing.Size(68, 17);
+            this.totalPayLabel.TabIndex = 16;
+            this.totalPayLabel.Text = "Total Pay";
+            // 
+            // averagePayTextBox
+            // 
+            this.averagePayTextBox.Location = new System.Drawing.Point(332, 288);
+            this.averagePayTextBox.Name = "averagePayTextBox";
+            this.averagePayTextBox.Size = new System.Drawing.Size(100, 22);
+            this.averagePayTextBox.TabIndex = 17;
+            // 
+            // averagePayLabel
+            // 
+            this.averagePayLabel.AutoSize = true;
+            this.averagePayLabel.Location = new System.Drawing.Point(332, 258);
+            this.averagePayLabel.Name = "averagePayLabel";
+            this.averagePayLabel.Size = new System.Drawing.Size(89, 17);
+            this.averagePayLabel.TabIndex = 18;
+            this.averagePayLabel.Text = "Average Pay";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.averagePayLabel);
+            this.Controls.Add(this.averagePayTextBox);
+            this.Controls.Add(this.totalPayLabel);
+            this.Controls.Add(this.toalPiecesLabel);
+            this.Controls.Add(this.totalPayTextBox);
+            this.Controls.Add(this.totalPiecesTextBox);
+            this.Controls.Add(this.amountEarnedLabel);
+            this.Controls.Add(this.amountEarned2TextBox);
+            this.Controls.Add(this.amountEarned1TextBox);
+            this.Controls.Add(this.piecesCompletedLabel);
+            this.Controls.Add(this.piecesCompleted2TextBox);
+            this.Controls.Add(this.piecesCompleted1TextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.name2TextBox);
+            this.Controls.Add(this.name1TextBox);
+            this.Controls.Add(this.clearAllButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.summaryButton);
+            this.Controls.Add(this.calculateButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button summaryButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button clearAllButton;
+        private System.Windows.Forms.TextBox name1TextBox;
+        private System.Windows.Forms.TextBox name2TextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox piecesCompleted1TextBox;
+        private System.Windows.Forms.TextBox piecesCompleted2TextBox;
+        private System.Windows.Forms.Label piecesCompletedLabel;
+        private System.Windows.Forms.TextBox amountEarned1TextBox;
+        private System.Windows.Forms.TextBox amountEarned2TextBox;
+        private System.Windows.Forms.Label amountEarnedLabel;
+        private System.Windows.Forms.TextBox totalPiecesTextBox;
+        private System.Windows.Forms.TextBox totalPayTextBox;
+        private System.Windows.Forms.Label toalPiecesLabel;
+        private System.Windows.Forms.Label totalPayLabel;
+        private System.Windows.Forms.TextBox averagePayTextBox;
+        private System.Windows.Forms.Label averagePayLabel;
+    }
+}
+
---
+++ 4.6.cs.piecework/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Project: Piecework
+ * Programmer: Brendan Hawk
+ * Date:10/19/2020
+ * Description: User puts in their name and the amount of pieces that they made and depending on their amount they get paid more per piece. 
+ */
+namespace _4._6.cs.piecework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+            int pieces1;
+            int pieces2;
+            decimal earned1;
+            decimal earned2;
+            decimal totalPieces;
+            decimal totalPay;
+            decimal averagePay;
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //Declare the variables and set them equal to the correct textboxes
+
+           
+
+            pieces1 = int.Parse(piecesCompleted1TextBox.Text);
+            pieces2 = int.Parse(piecesCompleted2TextBox.Text);
+
+
+            // Calculate the earned amount for first person
+
+            if (pieces1 <= 199) {
+                earned1 = (decimal)(pieces1 * 0.5);
+            }
+            if (pieces1 >= 200 || pieces1 <= 399) {
+                earned1 = (decimal)(pieces1 * 0.55);
+            }
+            if (pieces1 >= 400 || pieces1 <= 599) {
+                earned1 = (decimal)(pieces1 * 0.6);
+            }
+            if (pieces1 <= 600)
+            {
+                earned1 = (decimal)(pieces1 * 0.65);
+            }
+
+            // Calculate the earned amount for second person
+
+            if (pieces2 <= 199)
+            {
+                earned2 = (decimal)(pieces2 * 0.5);
+            }
+            if (pieces2 >= 200 || pieces2 <= 399)
+            {
+                earned2 = (decimal)(pieces2 * 0.55);
+            }
+            if (pieces2 >= 400 || pieces2 <= 599)
+            {
+                earned2 = (decimal)(pieces2 * 0.6);
+            }
+            if (pieces2 <= 600)
+            {
+                earned2 = (decimal)(pieces2 * 0.65);
+            }
+            piecesCompleted1TextBox.Text = pieces1.ToString();
+            piecesCompleted2TextBox.Text = pieces2.ToString();
+            amountEarned1TextBox.Text = earned1.ToString("C");
+            amountEarned2TextBox.Text = earned2.ToString("C");
+        }
+
+        private void summaryButton_Click(object sender, EventArgs e)
+        {
+            // Calculate the total pieces, total pay, and average pay per person
+
+            totalPieces = pieces1 + pieces2;
+            totalPay = earned1 + earned2;
+            averagePay = totalPay / 2;
+
+            totalPiecesTextBox.Text = totalPieces.ToString();
+            totalPayTextBox.Text = totalPay.ToString("C");
+            averagePayTextBox.Text = averagePay.ToString("C");
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //Clear the name and number of pieces for the current employee
+
+            name1TextBox.Clear();
+            name2TextBox.Clear();
+            piecesCompleted1TextBox.Clear();
+            piecesCompleted2TextBox.Clear();
+
+        }
+
+        private void clearAllButton_Click(object sender, EventArgs e)
+        {
+            //Clear the summary totals after confirming the operation with the user
+
+            totalPiecesTextBox.Clear();
+            totalPayTextBox.Clear();
+            averagePayTextBox.Clear();
+
+        }
+    }
+}
